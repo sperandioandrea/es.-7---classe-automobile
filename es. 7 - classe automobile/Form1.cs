62ed51abb43b6893e773f68dd6777e0c94e6c11c
andrea <sperandio.andrea.studente@itispaleocapa.it>
2023-10-18 22:49:01 +0200

    Programma

--- es. 7 - classe automobile/Form1.cs
+++ es. 7 - classe automobile/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using es._7___classe_automobile.Properties;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,66 @@ namespace es._7___classe_automobile
 {
     public partial class Form1 : Form
     {
+        Automobile auto;
         public Form1()
         {
             InitializeComponent();
+            Automobile auto = new Automobile();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            auto.Velocità = 10;
+            auto.Marcia = 1;
+            auto.MotoreAcceso = true;
+        }
+
+
+        //BOTTONE ACCENSIONE
+        private void button1_Click(object sender, EventArgs e)
+        {
+            auto.AccendiMotore();
+            listView1.Items.Clear();
+            listView1.Items.Add("Motore acceso");
+        }
+        //BOTTONE SPEGNIMENTO
+        private void button2_Click(object sender, EventArgs e)
+        {
+            auto.SpegniMotore();
+            listView1.Items.Clear();
+            listView1.Items.Add("Motore spento");
+        }
+      
+        
+        //BOTTONE VELOCITA' +
+        private void button3_Click(object sender, EventArgs e)
+        {
+            auto.Accellerare();
+            listView1.Items.Clear();
+            listView1.Items.Add("Velocità:");
+        }
+        //BOTTONE VELOCITA' -
+        private void button4_Click(object sender, EventArgs e)
+        {
+            auto.Decelerare();
+            listView1.Items.Clear();
+            listView1.Items.Add("Velocità:");
+        }
+
+
+        //MARCIA +
+        private void button5_Click(object sender, EventArgs e)
+        {
+            auto.AumentaMarcia();
+            listView1.Items.Clear();
+            listView1.Items.Add("Marcia:");
+        }
+        //MARCIA -
+        private void button6_Click(object sender, EventArgs e)
+        {
+            auto.DiminuzioneMarcia();
+            listView1.Items.Clear();
+            listView1.Items.Add("Marcia:");
         }
     }
 }
--- es. 7 - classe automobile/Properties/Automobile.cs
+++ es. 7 - classe automobile/Properties/Automobile.cs
@@ -6,7 +6,73 @@ using System.Threading.Tasks;
 
 namespace es._7___classe_automobile.Properties
 {
-    internal class Automobile
+    class Automobile
     {
+        //attributi
+        private int velocità;
+        private int marcia;
+        private bool motoreAcceso;
+
+        //costruttori
+        public int Velocità
+        {
+            get { return velocità; }
+            set { velocità = value; }
+        }
+        public int Marcia
+        {
+            get { return marcia; }  
+            set { marcia = value; }
+        }
+        public bool MotoreAcceso
+        {
+            get { return motoreAcceso; }
+            set {  motoreAcceso = value; }
+        }
+
+
+        //ACCENSIONE E SPEGNIMENTO MOTORE
+        public void AccendiMotore()
+        {
+            motoreAcceso = true;
+        }
+        public void SpegniMotore()
+        {
+            motoreAcceso = false;
+        }
+
+
+        //ACCELERARE E DECELERARE
+        public void Accellerare()
+        {
+            if(motoreAcceso)
+            {
+                velocità += 10;
+            }
+        }
+        public void Decelerare()
+        {
+            if(motoreAcceso && velocità > 0)
+            {
+                velocità -= 10;
+            }
+        }
+
+
+        //AUMENTO E DIMINUZIONE MARCIA
+        public void AumentaMarcia()
+        {
+            if (motoreAcceso && marcia < 5)
+            {
+                marcia++;
+            }
+        }
+        public void DiminuzioneMarcia()
+        {
+            if(motoreAcceso && marcia > 1)
+            {
+                marcia--;
+            }
+        }
     }
 }
